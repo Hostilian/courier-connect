deab5387bc95aec16c540dcd051db5f1ee6be474
Hostilian <77862924+Hostilian@users.noreply.github.com>
2025-10-15 03:11:36 +0200

    m

---
+++ lib/auth.ts
@@ -0,0 +1,27 @@
+import { jwtVerify } from 'jose'
+import { NextRequest } from 'next/server'
+
+const JWT_SECRET = process.env.JWT_SECRET
+
+interface UserPayload {
+  userId: string
+  role: 'courier' | 'admin'
+}
+
+export async function getAuth(request: NextRequest) {
+  const token = request.headers.get('authorization')?.split(' ')[1]
+
+  if (!token || !JWT_SECRET) {
+    return { user: null }
+  }
+
+  try {
+    const { payload } = await jwtVerify(
+      token,
+      new TextEncoder().encode(JWT_SECRET)
+    )
+    return { user: payload as UserPayload }
+  } catch (error) {
+    return { user: null }
+  }
+}
